067219272c4e421b08b6419d293d716d64d077dc
manuel <rai.rusianaemmanuel@gmail.com>
2026-01-26 19:28:04 +0800

    hello

--- api/index.js
+++ api/index.js
@@ -4,13 +4,13 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 //! ROUTES
-import notificationRouter from "./routes/notification.route.js";
-import userRouter from "./routes/user.routes.js";
-import concernRouter from "./routes/concern.route.js";
-import categoryRouter from "./routes/category.route.js";
-import announcementRouter from "./routes/announcement.route.js";
+import notificationRouter from "../routes/notification.route.js";
+import userRouter from "../routes/user.routes.js";
+import concernRouter from "../routes/concern.route.js";
+import categoryRouter from "../routes/category.route.js";
+import announcementRouter from "../routes/announcement.route.js";
 
-import summonRouter from "./routes/summon.route.js";
+import summonRouter from "../routes/summon.route.js";
 
 const app = express();
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -23,21 +23,21 @@ app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.json({ status: "ok", message: "Server is running!" });
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/users/", userRouter);
+app.use("/api/users/", userRouter);
 
-app.use("/concern/", concernRouter);
+app.use("/api/concern/", concernRouter);
 
-app.use("/category/", categoryRouter);
-app.use("/summon/", summonRouter);
+app.use("/api/category/", categoryRouter);
+app.use("/api/summon/", summonRouter);
 
-app.use("/notification/", notificationRouter);
+app.use("/api/notification/", notificationRouter);
 
-app.use("/announcements/", announcementRouter);
+app.use("/api/announcements/", announcementRouter);
 
 export default serverless(app);
